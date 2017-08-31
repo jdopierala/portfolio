9b92c7010aeb5c3854ba8aa2101834fb649e1969
jdopierala <justin.dopierala@gmail.com>
2017-08-31 12:59:07 +1000

    removed nav-link.js, changed design slightly

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,11 @@ import Layout from '../layout';
 
 export default () => (
   <Layout>
-    <p>boilerplate/filling/2pce feed...</p>
+    <h2>Welcome.</h2>
+    <p>I'm Justin.</p>
+    <p>Web Developer in training.</p>
+    <p>Melbourne based.</p>
+    <p>Documenting what I want to share with the world.</p>
+    <p>Best wishes to you!</p>
   </Layout>
 );
